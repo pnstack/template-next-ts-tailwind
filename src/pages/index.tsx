1744d685cb7514dfde2ab3de7ce50e0a952cfdeb
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-28 22:30:50 +0700

    Config lint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <>
-      <div className="w-screen h-screen flex items-center justify-center">
-        <h1 className="font-bold text-6xl text-gray-800">Hello</h1>
+      <div className="flex h-screen w-screen items-center justify-center">
+        <h1 className="text-6xl font-bold text-gray-800">Hello</h1>
       </div>
     </>
   );
