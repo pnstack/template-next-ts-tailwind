5efb90dd869891c3a82698f9f4895cfb58ab41d9
Phạm Văn Nguyên <npv2k1@gmail.com>
2024-04-06 08:30:33 +0700

    feat: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { ReactElement } from 'react';
+import { Provider } from 'react-redux';
 
 import AppContextProvider from '@/contexts/AppContext';
+import { store } from '@/redux';
 import { AppPropsWithLayout } from '@/types';
 
 import '@/styles/globals.scss';
@@ -8,6 +10,10 @@ import '@/styles/globals.scss';
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page: ReactElement) => page);
 
-  return <AppContextProvider>{getLayout(<Component {...pageProps} />)}</AppContextProvider>;
+  return (
+    <AppContextProvider>
+      <Provider store={store}>{getLayout(<Component {...pageProps} />)}</Provider>
+    </AppContextProvider>
+  );
 };
 export default App;
